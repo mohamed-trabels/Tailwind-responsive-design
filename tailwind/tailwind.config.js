f87ec63936f9d3f5149f3a9e4730312f0b6a21d3
mohamed-trabels <mohamed930trabelsi930@gmail.com>
2024-02-09 12:28:38 +0100

    done

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './build/**/*.html',
+    // Add more paths if needed
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Protest Riot'],
+      },
+    },
+  },
+  plugins: [],
+}
